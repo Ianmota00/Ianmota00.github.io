4e648f1dbed18d693f5827aac9db724943119fae
Ianmota00 <brsalvatore16@gmail.com>
2024-02-27 15:38:03 -0300

    alterar css e html

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,3 +1,16 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyDON-GHUbFL6goqOzQ2iaLmoTf_XnRK364",
+  authDomain: "sint-5508d.firebaseapp.com",
+  projectId: "sint-5508d",
+  storageBucket: "sint-5508d.appspot.com",
+  messagingSenderId: "20313037276",
+  appId: "1:20313037276:web:fa56e81bd5a04cd01b5719",
+  measurementId: "G-GBP7LP45EJ"
+};
+
+
+
 if (localStorage.getItem("token") == null) {
     alert("Você precisa estar logado para acessar essa página");
     window.location.href = "./assets/html/signin.html";
